6240e6ba488fe396ad58b49c35481cec45433153
Meruyert <mukhanova97@gmail.com>
2016-09-10 01:10:22 +0600

    Update TennisMatch.java

--- TennisMatch.java
+++ TennisMatch.java
@@ -4,11 +4,29 @@ public class TennisMatch {
   public static void main(String args[]) throws Exception{
     int a = 0;
     int b = 0;
+    int aa = 0;
+    int bb = 0;
+    String aaa = "PLAYER1";
+    String bbb = "PLAYER2";
     Scanner in = new Scanner(new File("MatchResults.txt"));
     System.out.println("INPUT:");
     while (in.hasNextLine()){
       String line = in.nextLine();
+      if (line.equals(aaa)) {
+        a+=15;
+      } else { b+=15;}
+      if (a >= 60){
+        aa++;
+        b = 0;
+        a = 0;
+      } else if (b >= 60) {
+        bb++;
+        a = 0;
+        b = 0;
+      }
       System.out.println(line);
     }
+    System.out.println("OUTPUT:");
+    System.out.println(aa + ":" + bb+" | " + a + ":" + b);
   }
 }
